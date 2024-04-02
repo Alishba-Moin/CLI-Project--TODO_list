88eaaacb19af1fc1fc56eb2f59eedb57bf772e5b
Alishba-Moin <159716625+Alishba-Moin@users.noreply.github.com>
2024-04-02 16:42:23 +0500

    push my  todo list project on github
    this is cli-based todo list project

---
+++ index.js
@@ -0,0 +1,70 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+let todos = [];
+async function addTodo() {
+    let loop = true;
+    while (loop) {
+        let answers = await inquirer.prompt([
+            {
+                name: "todo",
+                type: "input",
+                message: "What do you want to add in your todo?",
+                validate: function (input) {
+                    return input.trim() !== " " ? true : "Please enter a valid items in to-do list.";
+                }
+            },
+            {
+                name: "category",
+                type: "list",
+                message: "Select a category",
+                choices: ["personal", "work", "school", "task"]
+            },
+            {
+                name: "addmore",
+                type: "confirm",
+                message: "Do you want to add more todo?",
+                default: false
+            }
+        ]);
+        let newTodo = {
+            Todo: answers.todo,
+            category: answers.category
+        };
+        todos.push(newTodo);
+        console.log(`Todo item ${answers.todo} added to ${answers.category} Succesfully.`);
+        loop = answers.loop;
+    }
+    displaytodos();
+}
+function displaytodos() {
+    if (todos.length > 0) {
+        console.log("Your todo list:");
+        todos.forEach((todo, index) => {
+            console.log(`${index + 1}.${chalk.italic(colorCategory(todo.category)),
+                (todo.Todo)} (${todo.category})`);
+        });
+    }
+    else {
+        console.log("No todos found!");
+    }
+}
+function colorCategory(category) {
+    switch (category) {
+        case "personal":
+            return chalk.cyan;
+        case "work":
+            return chalk.yellow;
+        case "school":
+            return chalk.green;
+        case "task":
+            return chalk.blue;
+        default:
+            return chalk.white;
+    }
+}
+async function main() {
+    console.log(chalk.bold.bgMagentaBright `"Welcome to the TO-DO list application!"`);
+    await addTodo();
+}
+main();
